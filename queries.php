8bd5ae8be5e396d6ecc1d762d62e90d5eb02174f
Aulia <mutiaranauliadz@gmail.com>
2024-04-15 12:52:40 +0700

    1504_12.52_FinalExec

--- queries.php
+++ queries.php
@@ -1,5 +1,24 @@
 <?php
-include 'db_connect.php'; // Include the database connection file
+
+// Function to connect to the database
+function connectDB()
+{
+    // Database credentials
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "DB_DCALC";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
 
 // Function to execute a query
 function executeQuery($sql) {
